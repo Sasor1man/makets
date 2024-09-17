4aa939ec2a506ad872576b3b46e89aeb6f47f932
Sasoriman <pavelbomberov@gmail.com>
2024-09-17 18:59:44 +0300

    feat: corrected index, start catalog page

--- climat_systems/index.js
+++ climat_systems/index.js
@@ -1,15 +1,22 @@
 const menu = document.getElementById('menu_cont');
 const menuButton = document.getElementById('menuButton');
 
-function dropDown (element) {
+function dropDown(element) {
     element.classList.remove('hide');
-   element.classList.add('show');   
+    element.classList.add('show');
+}
+
+const disappear = (element) => {
+    element.classList.remove('show');
+    element.classList.add('hide');
 }
 
 menuButton.onclick = function () {
     dropDown(menu);
 }
 
-document.addEventListener('click', function() {
-    if(!menu.contains(event.target))
+document.addEventListener('click', function (event) {
+    if (!menuButton.contains(event.target)) {
+        disappear(menu);
+    };
 })
